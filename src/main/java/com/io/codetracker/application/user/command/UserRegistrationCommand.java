29461d5eb89b02e808db6d84489250769f756f11
dexxf <dexter101306@gmail.com>
2026-01-17 09:00:38 +0800

    feat: add command and DTO for UserRegistration

---
+++ src/main/java/com/io/codetracker/application/user/command/UserRegistrationCommand.java
@@ -0,0 +1,10 @@
+package com.io.codetracker.application.user.command;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record UserRegistrationCommand(@NotBlank String userId, @NotBlank String firstName,@NotBlank String lastName, @NotBlank String username,
+        @NotBlank String phoneNumber,@NotBlank String gender,@NotNull LocalDate birthday,@NotBlank String profileUrl, String bio) {
+}
