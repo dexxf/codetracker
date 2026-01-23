57ef7a7407f01d976408570019328b8f6def82ab
dexxf <dexter101306@gmail.com>
2026-01-23 17:40:17 +0800

    feat: add UserAppRepository interface and UserAppRepository implementation

---
+++ src/main/java/com/io/codetracker/application/user/port/out/UserAppRepository.java
@@ -0,0 +1,10 @@
+package com.io.codetracker.application.user.port.out;
+
+import com.io.codetracker.domain.user.entity.User;
+
+import java.util.Optional;
+
+public interface UserAppRepository {
+    void save(User user);
+    Optional<User> findByUserId(String userId);
+}
