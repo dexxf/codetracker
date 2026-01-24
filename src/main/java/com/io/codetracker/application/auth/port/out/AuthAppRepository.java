2e37d556235366c9bfa4435c77d8f416e5a2dc7c
dexxf <dexter101306@gmail.com>
2026-01-24 10:16:55 +0800

    feat: add AuthAppRepository interface

---
+++ src/main/java/com/io/codetracker/application/auth/port/out/AuthAppRepository.java
@@ -0,0 +1,11 @@
+package com.io.codetracker.application.auth.port.out;
+
+import com.io.codetracker.domain.auth.entity.Auth;
+
+import java.util.Optional;
+
+public interface AuthAppRepository {
+    void save(Auth auth);
+    boolean emailExists(String email);
+    Optional<Auth> findByEmail(String email);
+}
