20de2242b41420f858f134095bc81a318f539617
dexxf <dexter101306@gmail.com>
2026-01-24 10:23:06 +0800

    feat(security): configure security filter chain, add auth provider

---
+++ src/main/java/com/io/codetracker/infrastructure/auth/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.io.codetracker.infrastructure.auth.config;
+
+
+import com.io.codetracker.adapter.auth.out.security.BCryptPasswordHasher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+        private final UserDetailsService userDetailsService;
+
+        public SecurityConfig (UserDetailsService userDetailsService) {
+            this.userDetailsService = userDetailsService;
+        }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(e -> e.disable())
+                .formLogin(e -> e.disable())
+                .logout(e -> e.disable())
+                .authenticationProvider(authenticationProvider())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/login","/api/auth/register", "/api/user/register").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public BCryptPasswordHasher passwordEncoder() {
+        return new BCryptPasswordHasher(bCryptPasswordEncoder());
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider () {
+        DaoAuthenticationProvider dao = new DaoAuthenticationProvider(userDetailsService);
+        dao.setPasswordEncoder(passwordEncoder());
+        return dao;
+    }
+
+}
