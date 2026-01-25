7221488262bfcdd683508485a8cd52ead20c96d4
Dexter Facelo <Dexter101306@gmail.com>
2026-01-25 13:59:54 +0800

    feat(user): implement user profile update (#2)
    * feat: add UserProfileUpdaterResult
    
    * feat: add UserNotFoundException
    
    * feat: add UserProfileUpdater
    
    * feat: add UserProfileUpdater as a Spring bean
    
    * feat: add command and response DTO for UserProfileService
    
    * feat: add UserProfileService
    
    * feat: add PUT /api/user/profile endpoint for profile update

---
+++ src/main/java/com/io/codetracker/application/user/response/UserProfileResponseDTO.java
@@ -0,0 +1,32 @@
+package com.io.codetracker.application.user.response;
+
+import com.io.codetracker.domain.user.entity.User;
+
+import java.util.Map;
+
+public record UserProfileResponseDTO(boolean success, String message, Map<String, Object> userDetails , Map<String, Object> errorList) {
+
+    public static UserProfileResponseDTO ok(User user) {
+        return new UserProfileResponseDTO(true, "Successfully updated user profile.",
+                Map.ofEntries(
+                        Map.entry("userId", user.getUserId()),
+                        Map.entry("firstName", user.getFirstName()),
+                        Map.entry("lastName", user.getLastName()),
+                        Map.entry("gender", user.getGender()),
+                        Map.entry("phoneNumber",user.getPhoneNumber().getValue()),
+                        Map.entry("bio", user.getBio()),
+                        Map.entry("birthday", user.getBirthday().getValue()),
+                        Map.entry("profileUrl", user.getProfileUrl())
+                ),
+                Map.of());
+    }
+
+    public static UserProfileResponseDTO fail(Map<String, Object> errors) {
+        return new UserProfileResponseDTO(false,"Failed to update user profile.",Map.of() ,errors);
+    }
+
+    public static UserProfileResponseDTO fail(String message) {
+        return new UserProfileResponseDTO(false, message,Map.of() ,Map.of());
+    }
+
+}
---
+++ src/main/java/com/io/codetracker/application/user/service/UserProfileService.java
@@ -0,0 +1,40 @@
+package com.io.codetracker.application.user.service;
+
+import com.io.codetracker.application.user.command.UserProfileCommand;
+import com.io.codetracker.application.user.response.UserProfileResponseDTO;
+import com.io.codetracker.application.user.port.out.UserAppRepository;
+import com.io.codetracker.domain.user.entity.User;
+import com.io.codetracker.domain.user.result.UserProfileUpdaterResult;
+import com.io.codetracker.domain.user.service.UserProfileUpdater;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public final class UserProfileService {
+       private final UserAppRepository repository;
+       private final UserProfileUpdater userProfileUpdater;
+
+       public UserProfileService (UserProfileUpdater userProfileUpdater,UserAppRepository repository) {
+           this.repository = repository;
+           this.userProfileUpdater = userProfileUpdater;
+       }
+
+       public UserProfileResponseDTO execute(UserProfileCommand command) {
+           Optional<User> userOpt = repository.findByUserId(command.userId());
+
+           if(userOpt.isEmpty())
+               return UserProfileResponseDTO.fail("User not found.");
+
+           User user = userOpt.get();
+
+           UserProfileUpdaterResult userProfileUpdaterResult = userProfileUpdater.update(user, command.firstName(), command.lastName(),
+                   command.gender(), command.phoneNumber(), command.bio(), command.birthday());
+
+           if(userProfileUpdaterResult.hasErrors())
+               return UserProfileResponseDTO.fail(userProfileUpdaterResult.errors());
+
+           repository.save(user);
+           return UserProfileResponseDTO.ok(user);
+       }
+}
