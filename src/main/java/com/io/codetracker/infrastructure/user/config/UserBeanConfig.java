1c80a87ecd7e5823895887ea29da10963c9ef649
dexxf <dexter101306@gmail.com>
2026-01-25 13:39:17 +0800

    feat: add UserProfileUpdater as a Spring bean

--- src/main/java/com/io/codetracker/infrastructure/user/config/UserBeanConfig.java
+++ src/main/java/com/io/codetracker/infrastructure/user/config/UserBeanConfig.java
@@ -3,6 +3,7 @@ package com.io.codetracker.infrastructure.user.config;
 import com.io.codetracker.domain.user.factory.UserFactory;
 import com.io.codetracker.domain.user.service.UserCreationService;
 
+import com.io.codetracker.domain.user.service.UserProfileUpdater;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,11 @@ public class UserBeanConfig {
     @Bean
     public UserCreationService userCreationService (@Qualifier("defaultUserFactory") UserFactory factory) {
         return new UserCreationService(factory);
+    }
+
+    @Bean
+    public UserProfileUpdater userProfileUpdater () {
+        return new UserProfileUpdater();
     }
 
 }
