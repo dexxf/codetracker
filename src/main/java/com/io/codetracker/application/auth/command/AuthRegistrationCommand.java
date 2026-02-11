c567cb0f67fc6f7a845ee1ade6b9c3e9c522b81d
dexxf <dexter101306@gmail.com>
2026-02-11 10:07:00 +0800

    chore: remove auth password registration endpoint and services

--- src/main/java/com/io/codetracker/application/auth/command/AuthRegistrationCommand.java
+++
@@ -1,6 +0,0 @@
-package com.io.codetracker.application.auth.command;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record AuthRegistrationCommand(@NotBlank String email,@NotBlank String username,@NotBlank String rawPassword, String role) {
-}
--- src/main/java/com/io/codetracker/application/auth/service/AuthRegistration.java
+++ src/main/java/com/io/codetracker/application/auth/service/AuthRegistration.java
@@ -1,7 +1,6 @@
 package com.io.codetracker.application.auth.service;
 
 import com.io.codetracker.application.auth.command.AuthRegisterOAuthCommand;
-import com.io.codetracker.application.auth.command.AuthRegistrationCommand;
 import com.io.codetracker.application.auth.response.AuthRegistrationResponseDTO;
 import com.io.codetracker.application.auth.port.out.AuthAppRepository;
 import com.io.codetracker.application.auth.port.out.UserRegistrationPort;
@@ -22,26 +21,6 @@ public final class AuthRegistration {
         this.authCreationService = authCreationService;
         this.authAppRepository = authAppRepository;
         this.userRegistration = userRegistration;
-    }
-
-    public AuthRegistrationResponseDTO execute(AuthRegistrationCommand command) {
-        if(authAppRepository.emailExists(command.email())) {
-            return AuthRegistrationResponseDTO.fail("Email taken.");
-        }
-
-        String userId = userRegistration.createShallowUser();
-        Result<Auth, AuthCreationResult> authCreationResult =
-        authCreationService.createAuth(userId, command.email(), command.username(), command.rawPassword(), command.role().toUpperCase());
-
-        if(!authCreationResult.success()) {
-            return AuthRegistrationResponseDTO.fail(authCreationResult.error().getMessage());
-        }
-
-        Auth auth = authCreationResult.data();
-
-
-        authAppRepository.save(authCreationResult.data());
-        return AuthRegistrationResponseDTO.success(auth);
     }
 
     public AuthRegistrationResponseDTO registerWithOAuth(AuthRegisterOAuthCommand command) {
