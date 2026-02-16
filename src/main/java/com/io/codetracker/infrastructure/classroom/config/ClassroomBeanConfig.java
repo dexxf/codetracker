28cf70a6a15929465b061e2898eaf57a16384020
dexxf <dexter101306@gmail.com>
2026-02-16 22:20:01 +0800

    feat: add ClassroomCreationService as Spring bean

--- src/main/java/com/io/codetracker/infrastructure/classroom/config/ClassroomBeanConfig.java
+++ src/main/java/com/io/codetracker/infrastructure/classroom/config/ClassroomBeanConfig.java
@@ -6,6 +6,10 @@ import java.security.SecureRandom;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.io.codetracker.domain.classroom.factory.ClassroomFactory;
+import com.io.codetracker.domain.classroom.repository.ClassroomUserDomainPort;
+import com.io.codetracker.domain.classroom.service.ClassroomCreationService;
+
 @Configuration
 public class ClassroomBeanConfig {
 
@@ -14,4 +18,9 @@ public class ClassroomBeanConfig {
         return new SecureRandom();
     }
 
+    @Bean
+    public ClassroomCreationService classroomCreateService(ClassroomFactory factory, ClassroomUserDomainPort classroomUserDomainPort) {
+        return new ClassroomCreationService(factory, classroomUserDomainPort);
+    }
+    
 }
