647935b90f988a355c621800fb0d064caac00165
dexxf <dexter101306@gmail.com>
2026-03-05 23:38:28 +0800

    feat: add ClassroomJoinService as spring bean

--- src/main/java/com/io/codetracker/infrastructure/classroom/config/ClassroomBeanConfig.java
+++ src/main/java/com/io/codetracker/infrastructure/classroom/config/ClassroomBeanConfig.java
@@ -1,10 +1,12 @@
 package com.io.codetracker.infrastructure.classroom.config;
 
-
 import java.security.SecureRandom;
 
 import com.io.codetracker.domain.classroom.factory.ClassroomStudentFactory;
+import com.io.codetracker.domain.classroom.repository.ClassroomDomainRepository;
+import com.io.codetracker.domain.classroom.repository.ClassroomSettingsDomainRepository;
 import com.io.codetracker.domain.classroom.repository.ClassroomStudentDomainRepository;
+import com.io.codetracker.domain.classroom.service.ClassroomJoinService;
 import com.io.codetracker.domain.classroom.service.ClassroomStudentCreationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,5 +31,11 @@ public class ClassroomBeanConfig {
     @Bean
     public ClassroomStudentCreationService classroomStudentCreationService(ClassroomStudentFactory factory, ClassroomStudentDomainRepository repository) {
         return new ClassroomStudentCreationService(factory,repository);
+    }
+
+    @Bean
+    public ClassroomJoinService classroomJoinService(ClassroomDomainRepository classroomDomainRepository, ClassroomSettingsDomainRepository classroomSettingsDomainRepository,
+                                                     ClassroomStudentDomainRepository classroomStudentDomainRepository) {
+        return new ClassroomJoinService(classroomDomainRepository,classroomSettingsDomainRepository, classroomStudentDomainRepository);
     }
 }
