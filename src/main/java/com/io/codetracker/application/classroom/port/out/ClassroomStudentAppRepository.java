5513fa97f50f96bfc87c0f333d8390185242c4f8
dexxf <dexter101306@gmail.com>
2026-03-06 10:12:22 +0800

    chore: add findActiveEnrollmentsWithActiveClassroom and countByClassroomIds method in ClassroomStudentAppRepository interface

--- src/main/java/com/io/codetracker/application/classroom/port/out/ClassroomStudentAppRepository.java
+++ src/main/java/com/io/codetracker/application/classroom/port/out/ClassroomStudentAppRepository.java
@@ -2,6 +2,11 @@ package com.io.codetracker.application.classroom.port.out;
 
 import com.io.codetracker.domain.classroom.entity.ClassroomStudent;
 
+import java.util.List;
+import java.util.Map;
+
 public interface ClassroomStudentAppRepository {
     boolean save(ClassroomStudent classroomStudent);
+    List<ClassroomStudent> findActiveEnrollmentsWithActiveClassroom(String studentUserId);
+    Map<String, Integer> countByClassroomIds(List<String> classroomIds);
 }
