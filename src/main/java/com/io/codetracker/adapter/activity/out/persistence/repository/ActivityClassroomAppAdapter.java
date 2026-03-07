6323b1bab34809487d2e60eb1bc7374c531c254a
dexxf <dexter101306@gmail.com>
2026-03-07 12:50:49 +0800

    feat: implement ActivityClassroomAppAdapter with existsByClassroomId method

---
+++ src/main/java/com/io/codetracker/adapter/activity/out/persistence/repository/ActivityClassroomAppAdapter.java
@@ -0,0 +1,18 @@
+package com.io.codetracker.adapter.activity.out.persistence.repository;
+
+import com.io.codetracker.application.activity.port.out.ActivityClassroomAppPort;
+import com.io.codetracker.infrastructure.classroom.persistence.repository.JpaClassroomRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@AllArgsConstructor
+public class ActivityClassroomAppAdapter implements ActivityClassroomAppPort {
+
+    JpaClassroomRepository jpa;
+
+    @Override
+    public boolean existsByClassroomId(String s) {
+        return jpa.existsByClassroomId(s);
+    }
+}
