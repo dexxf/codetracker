c8b074dce7534e18ae6cff30503e15c4348df168
dexxf <dexter101306@gmail.com>
2026-03-07 12:49:08 +0800

    feat: implement ActivityAppRepository with findByClassroomId method

--- src/main/java/com/io/codetracker/infrastructure/activity/persistence/repository/JpaActivityRepository.java
+++ src/main/java/com/io/codetracker/infrastructure/activity/persistence/repository/JpaActivityRepository.java
@@ -3,6 +3,9 @@ package com.io.codetracker.infrastructure.activity.persistence.repository;
 import com.io.codetracker.infrastructure.activity.persistence.entity.ActivityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JpaActivityRepository extends JpaRepository<ActivityEntity, String> {
     boolean existsByClassroomIdAndActivityId(String classroomId, String activityId);
+    List<ActivityEntity> findByClassroomIdAndCreatedByProfessorId(String classroomId, String createdByProfessorId);
 }
