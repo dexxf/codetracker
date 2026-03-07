9c985f994bce64262c63329db05f2d280bdb620d
dexxf <dexter101306@gmail.com>
2026-03-07 12:46:32 +0800

    feat: add ActivityAppRepository and ActivityClassroomAppPort

--- src/main/java/com/io/codetracker/application/activity/port/out/ActivityAppRepository.java
+++ src/main/java/com/io/codetracker/application/activity/port/out/ActivityAppRepository.java
@@ -2,6 +2,9 @@ package com.io.codetracker.application.activity.port.out;
 
 import com.io.codetracker.domain.activity.entity.Activity;
 
+import java.util.List;
+
 public interface ActivityAppRepository {
     Activity save(Activity data);
+    List<Activity> findByClassroomId(String classroomId, String instructorId);
 }
