493d076496ef2620575268c1ddb821f4ede67edf
Dexter Facelo <Dexter101306@gmail.com>
2026-03-08 15:17:50 +0800

    feat: add email and username in auth check endpoint (#24)

--- src/main/java/com/io/codetracker/adapter/auth/in/rest/AuthController.java
+++ src/main/java/com/io/codetracker/adapter/auth/in/rest/AuthController.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +20,17 @@ import com.io.codetracker.adapter.auth.out.security.AuthPrincipal;
 public class AuthController {
         
 @GetMapping("/check")
-public ResponseEntity<?> checkAuthentication(@AuthenticationPrincipal AuthPrincipal principal) {
-
+public ResponseEntity<Map<String, Object>> checkAuthentication(@AuthenticationPrincipal AuthPrincipal principal) {
     if (principal == null) {
         return ResponseEntity.ok(Map.of("authenticated", false));
     }
 
     return ResponseEntity.ok(Map.of(
-        "authenticated", true,
-        "authId", principal.getUsername(),
-        "roles", principal.getAuthorities().stream().map(e -> e.getAuthority()).toList()
+            "authenticated", true,
+            "authId", principal.getUsername(),
+            "roles", principal.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList(),
+            "email", principal.getEmail(),
+            "username", principal.getAuthUsername()
     ));
 }
 
