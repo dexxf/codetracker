8fccab6f45c3fc183adac45ad025bfadbc2a549f
dexxf <dexter101306@gmail.com>
2026-03-09 22:01:28 +0800

    feat: add UpdateActivityService as spring bean

--- src/main/java/com/io/codetracker/infrastructure/activity/config/ActivityConfig.java
+++ src/main/java/com/io/codetracker/infrastructure/activity/config/ActivityConfig.java
@@ -4,6 +4,7 @@ import com.io.codetracker.domain.activity.factory.ActivityFactory;
 import com.io.codetracker.domain.activity.repository.ActivityDomainRepository;
 import com.io.codetracker.domain.activity.repository.ActivityUserDomainPort;
 import com.io.codetracker.domain.activity.service.ActivityCreationService;
+import com.io.codetracker.domain.activity.service.UpdateActivityService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +14,10 @@ public class ActivityConfig {
     @Bean
     public ActivityCreationService activityCreationService(ActivityDomainRepository activityDomainRepository, ActivityFactory activityFactory, ActivityUserDomainPort activityUserDomainPort) {
         return new ActivityCreationService(activityDomainRepository,activityFactory, activityUserDomainPort);
+    }
+
+    @Bean
+    public UpdateActivityService validateEditActivityService(ActivityDomainRepository activityDomainRepository) {
+        return new UpdateActivityService(activityDomainRepository);
     }
 }
