8812c9c4745d0db3bb7ec8f3277f7e45360be7bf
dexxf <dexter101306@gmail.com>
2026-03-09 21:55:49 +0800

    feat: implement findByActivityId method in ActivityDomainRepository

--- src/main/java/com/io/codetracker/adapter/activity/out/persistence/repository/ActivityDomainRepositoryImpl.java
+++ src/main/java/com/io/codetracker/adapter/activity/out/persistence/repository/ActivityDomainRepositoryImpl.java
@@ -1,9 +1,13 @@
 package com.io.codetracker.adapter.activity.out.persistence.repository;
 
+import com.io.codetracker.adapter.activity.out.persistence.mapper.ActivityMapper;
+import com.io.codetracker.domain.activity.entity.Activity;
 import com.io.codetracker.domain.activity.repository.ActivityDomainRepository;
 import com.io.codetracker.infrastructure.activity.persistence.repository.JpaActivityRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
@@ -14,6 +18,11 @@ public class ActivityDomainRepositoryImpl implements ActivityDomainRepository {
     @Override
     public boolean existsByClassroomIdAndActivityId(String classroomId, String activityId) {
         return jpa.existsByClassroomIdAndActivityId(classroomId, activityId);
+    }
+
+    @Override
+    public Optional<Activity> findByActivityId(String activityId) {
+        return jpa.findById(activityId).map(ActivityMapper::toDomain);
     }
 
     @Override
