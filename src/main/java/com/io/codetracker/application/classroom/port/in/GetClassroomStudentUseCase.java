18d7463a474dc2076e3a7132408e140a1f047358
dexxf <dexter101306@gmail.com>
2026-03-13 17:29:51 +0800

    refactor: use GetClassroomStudentUseCase and return generic Result

---
+++ src/main/java/com/io/codetracker/application/classroom/port/in/GetClassroomStudentUseCase.java
@@ -0,0 +1,11 @@
+package com.io.codetracker.application.classroom.port.in;
+
+import com.io.codetracker.application.classroom.command.GetClassroomStudentCommand;
+import com.io.codetracker.application.classroom.result.ClassroomStudentData;
+import com.io.codetracker.common.result.Result;
+
+import java.util.List;
+
+public interface GetClassroomStudentUseCase {
+    Result<List<ClassroomStudentData>, String> execute(GetClassroomStudentCommand command);
+}
