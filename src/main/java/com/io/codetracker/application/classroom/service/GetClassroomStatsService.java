11a36071a2388e5649ab907905a64b437b17cdf2
dexxf <dexter101306@gmail.com>
2026-03-13 11:03:45 +0800

    feat: add app service for retrieving classroom stats

---
+++ src/main/java/com/io/codetracker/application/classroom/service/GetClassroomStatsService.java
@@ -0,0 +1,35 @@
+package com.io.codetracker.application.classroom.service;
+
+import org.springframework.stereotype.Service;
+
+import com.io.codetracker.application.classroom.command.ClassroomStatsCommand;
+import com.io.codetracker.application.classroom.port.in.response.GetClassroomStatsResponse;
+import com.io.codetracker.application.classroom.port.out.ClassroomActivityAppPort;
+import com.io.codetracker.application.classroom.port.out.ClassroomAppRepository;
+import com.io.codetracker.application.classroom.port.out.ClassroomStudentAppRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class GetClassroomStatsService {
+    
+
+    private final ClassroomAppRepository classroomAppRepository;
+    private final ClassroomStudentAppRepository classroomStudentAppRepository;
+    private final ClassroomActivityAppPort classroomActivityAppPort;
+
+    public GetClassroomStatsResponse execute(ClassroomStatsCommand command) {
+        boolean isOwner = classroomAppRepository.existsByClassroomIdAndInstructorUserId(command.classroomId(),command.userId());
+        if(!isOwner) {
+            return GetClassroomStatsResponse.fail(command.classroomId(), "You are not the owner of this classroom.");
+        }
+
+        long totalStudents = classroomStudentAppRepository.countByClassroomId(command.classroomId());
+        long totalActivities = classroomActivityAppPort.countByClassroomId(command.classroomId());
+        long totalActiveActivities = classroomActivityAppPort.countActiveActivitiesByClassroomId(command.classroomId());
+
+        return GetClassroomStatsResponse.success(command.classroomId(), totalStudents, totalActivities, totalActiveActivities);
+    }
+
+}
