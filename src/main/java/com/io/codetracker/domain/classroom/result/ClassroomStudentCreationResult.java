57610f5bb7cd69d23299df5295a3ce3152027ba4
dexxf <dexter101306@gmail.com>
2026-03-14 20:15:39 +0800

    refactor: remove message in ClassroomStudentCreationResult

--- src/main/java/com/io/codetracker/domain/classroom/result/ClassroomStudentCreationResult.java
+++ src/main/java/com/io/codetracker/domain/classroom/result/ClassroomStudentCreationResult.java
@@ -1,18 +1,8 @@
 package com.io.codetracker.domain.classroom.result;
 
 public enum ClassroomStudentCreationResult {
-    CLASSROOM_ID_INVALID("Classroom ID cannot be null or empty."),
-    STUDENT_USER_ID_INVALID("Student User ID cannot be null or empty."),
-    STATUS_INVALID("Student status cannot be null."),
-    USER_ALREADY_IN_CLASSROOM("User already in classroom.");
-
-    private final String message;
-
-    ClassroomStudentCreationResult(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    CLASSROOM_ID_INVALID,
+    STUDENT_USER_ID_INVALID,
+    STATUS_INVALID,
+    USER_ALREADY_IN_CLASSROOM;
 }
