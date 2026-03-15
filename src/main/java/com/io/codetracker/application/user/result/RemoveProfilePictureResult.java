15b21bda5088774a05d3d458fef1b76ce4cf197d
dexxf <dexter101306@gmail.com>
2026-03-15 16:25:59 +0800

    feat: add RemoveProfilePictureResult

---
+++ src/main/java/com/io/codetracker/application/user/result/RemoveProfilePictureResult.java
@@ -0,0 +1,9 @@
+package com.io.codetracker.application.user.result;
+
+public enum RemoveProfilePictureResult {
+    SUCCESS,
+    USER_NOT_FOUND,
+    NO_PROFILE_PICTURE,
+    MULTIPLE_ROWS_AFFECTED,
+    DELETE_FAILED
+}
