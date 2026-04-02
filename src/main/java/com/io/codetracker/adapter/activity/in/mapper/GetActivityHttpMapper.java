ece55e978cff8716ab45d5b4ea77a94dbc6ba272
dexxf <dexter101306@gmail.com>
2026-04-02 20:27:15 +0800

    feat: add studentToStatus and studentToMessage in GetActivityHttpMapper

--- src/main/java/com/io/codetracker/adapter/activity/in/mapper/GetActivityHttpMapper.java
+++ src/main/java/com/io/codetracker/adapter/activity/in/mapper/GetActivityHttpMapper.java
@@ -1,6 +1,7 @@
 package com.io.codetracker.adapter.activity.in.mapper;
 
 import com.io.codetracker.application.activity.error.GetClassroomOwnerActivityError;
+import com.io.codetracker.application.activity.error.GetClassroomStudentActivityError;
 import org.springframework.http.HttpStatus;
 
 public final class GetActivityHttpMapper {
@@ -19,6 +20,21 @@ public final class GetActivityHttpMapper {
         return switch (error) {
             case CLASSROOM_NOT_FOUND -> "Classroom does not exists";
             case USER_NOT_CLASSROOM_INSTRUCTOR -> "User is not the owner of this classroom";
+        };
+    }
+
+    public static HttpStatus studentToStatus(GetClassroomStudentActivityError error){
+        return switch (error) {
+            case CLASSROOM_NOT_FOUND -> HttpStatus.NOT_FOUND;
+            case USER_NOT_CLASSROOM_STUDENT -> HttpStatus.UNAUTHORIZED;
+            default -> HttpStatus.BAD_REQUEST;
+        };
+    }
+
+    public static String studentToMessage(GetClassroomStudentActivityError error){
+        return switch (error) {
+            case CLASSROOM_NOT_FOUND -> "Classroom does not exists";
+            case USER_NOT_CLASSROOM_STUDENT -> "User is not a student of this classroom";
         };
     }
 
