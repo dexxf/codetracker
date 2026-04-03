1c0ddd2b8d858dc6d6eb408fbafa4eb3fa2fe060
dexxf <dexter101306@gmail.com>
2026-04-03 10:05:54 +0800

    feat: add StudentActivityData

---
+++ src/main/java/com/io/codetracker/application/activity/result/StudentActivityData.java
@@ -0,0 +1,20 @@
+package com.io.codetracker.application.activity.result;
+
+import com.io.codetracker.domain.activity.entity.StudentActivity;
+import com.io.codetracker.domain.activity.valueObject.SubmissionStatus;
+
+public record StudentActivityData(
+        String activityId,
+        String userId,
+        SubmissionStatus submissionStatus,
+        String feedback
+) {
+    public static StudentActivityData from(StudentActivity studentActivity) {
+        return new StudentActivityData(
+                studentActivity.getActivityId(),
+                studentActivity.getUserId(),
+                studentActivity.getSubmissionStatus(),
+                studentActivity.getFeedback()
+        );
+    }
+}
