da1f931d27fd09430d18add64bf36c14cc734788
dexxf <dexter101306@gmail.com>
2026-04-04 20:25:11 +0800

    feat: add FindUnsubmittedRepository command, error, usecase

---
+++ src/main/java/com/io/codetracker/application/activity/error/FindStudentUnsubmittedRepositoryError.java
@@ -0,0 +1,7 @@
+package com.io.codetracker.application.activity.error;
+
+public enum FindStudentUnsubmittedRepositoryError {
+    ACTIVITY_NOT_FOUND,
+    USER_NOT_CLASSROOM_STUDENT,
+    CLASSROOM_NOT_FOUND
+}
