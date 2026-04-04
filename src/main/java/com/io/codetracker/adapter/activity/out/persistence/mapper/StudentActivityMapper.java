2824500a90631e578afdeac53682f3b84dbaf647
dexxf <dexter101306@gmail.com>
2026-04-04 20:27:58 +0800

    feat: add StudentActivityMapper

---
+++ src/main/java/com/io/codetracker/adapter/activity/out/persistence/mapper/StudentActivityMapper.java
@@ -0,0 +1,23 @@
+package com.io.codetracker.adapter.activity.out.persistence.mapper;
+
+import com.io.codetracker.domain.activity.entity.StudentActivity;
+import com.io.codetracker.infrastructure.activity.persistence.entity.StudentActivityEntity;
+
+public class StudentActivityMapper {
+
+    private StudentActivityMapper() {}
+
+    public static StudentActivity toDomain(StudentActivityEntity entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        return new StudentActivity(
+                entity.getStudentActivityId().toString(), // TODO: convert Student activity Id attribute in the domain entity to UUID to stay consistent
+                entity.getActivityEntity().getActivityId(),
+                entity.getUserEntity().getUserId(),
+                entity.getSubmissionStatus(),
+                entity.getFeedback()
+        );
+    }
+}
