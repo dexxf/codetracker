335f84693c2b9e4999af93f9d0bcd4040caa726b
Dexter Facelo <Dexter101306@gmail.com>
2026-07-19 10:08:35 +0800

    feat: add restClient bean

--- src/main/java/com/io/codetracker/infrastructure/auth/config/AuthBeanConfig.java
+++ src/main/java/com/io/codetracker/infrastructure/auth/config/AuthBeanConfig.java
@@ -2,8 +2,11 @@ package com.io.codetracker.infrastructure.auth.config;
 
 import com.io.codetracker.domain.auth.service.*;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -17,6 +20,17 @@ public class AuthBeanConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public RestClient restClient(@Value("${rest-client.connect-timeout-ms}") int connectTimeout, @Value("${rest-client.read-timeout-ms}") int readTimeout) {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(connectTimeout);
+        factory.setReadTimeout(readTimeout);
+
+        return RestClient.builder()
+                .requestFactory(factory)
+                .build();
     }
 
 }
