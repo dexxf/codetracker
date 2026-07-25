5999660d8683ca7c4705fafad20ab61a6eea73e2
Dexter Facelo <Dexter101306@gmail.com>
2026-07-25 13:46:38 +0800

    feat: add Announcement and AnnouncementAttachment persistence entity

---
+++ src/main/java/com/io/codetracker/infrastructure/announcement/persistence/entity/AnnouncementAttachmentEntity.java
@@ -0,0 +1,49 @@
+package com.io.codetracker.infrastructure.announcement.persistence.entity;
+
+import com.io.codetracker.domain.announcement.valueobject.AttachmentType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "announcement_attachment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class AnnouncementAttachmentEntity {
+
+    @Id
+    @Column(name = "attachment_id", nullable = false, updatable = false)
+    private UUID attachmentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "announcement_id", nullable = false, updatable = false)
+    private AnnouncementEntity announcement;
+
+    @Column(name = "url", nullable = false, length = 2048)
+    private String url;
+
+    @Column(name = "public_id", nullable = false, length = 500)
+    private String publicId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false, length = 30)
+    private AttachmentType type;
+
+    @Column(name = "display_image", nullable = false)
+    private boolean displayImage;
+}
