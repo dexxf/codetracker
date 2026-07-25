cc05b21fc47aa283e3cadf8d99853c306f6858e4
Dexter Facelo <Dexter101306@gmail.com>
2026-07-25 13:46:56 +0800

    feat: add JpaAnnouncementRepository

---
+++ src/main/java/com/io/codetracker/infrastructure/announcement/persistence/repository/JpaAnnouncementRepository.java
@@ -0,0 +1,9 @@
+package com.io.codetracker.infrastructure.announcement.persistence.repository;
+
+import com.io.codetracker.infrastructure.announcement.persistence.entity.AnnouncementEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JpaAnnouncementRepository extends JpaRepository<AnnouncementEntity, UUID> {
+}
