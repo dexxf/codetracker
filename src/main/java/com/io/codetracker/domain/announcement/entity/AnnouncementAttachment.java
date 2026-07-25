8d1be34f191951b3b399e1022ea58df81ecaebd3
Dexter Facelo <Dexter101306@gmail.com>
2026-07-25 16:21:11 +0800

    refactor: remove static method and displayImage and publicId attr in AnnouncementAttachment

--- src/main/java/com/io/codetracker/domain/announcement/entity/AnnouncementAttachment.java
+++ src/main/java/com/io/codetracker/domain/announcement/entity/AnnouncementAttachment.java
@@ -2,7 +2,6 @@ package com.io.codetracker.domain.announcement.entity;
 
 import com.io.codetracker.domain.announcement.exception.EmptyAttachmentPublicIdException;
 import com.io.codetracker.domain.announcement.exception.EmptyAttachmentUrlException;
-import com.io.codetracker.domain.announcement.exception.InvalidDisplayImageTypeException;
 import com.io.codetracker.domain.announcement.valueobject.AttachmentType;
 
 import java.util.Objects;
@@ -12,67 +11,21 @@ public final class AnnouncementAttachment {
 
     private final UUID attachmentId;
     private final String url;
-    private final String publicId;
     private final AttachmentType type;
-    private boolean displayImage;
 
-    private AnnouncementAttachment(
+    public AnnouncementAttachment(
             UUID attachmentId,
             String url,
             String publicId,
-            AttachmentType type,
-            boolean displayImage
+            AttachmentType type
     ) {
         this.attachmentId = Objects.requireNonNull(attachmentId);
         this.type = Objects.requireNonNull(type);
-        this.displayImage = displayImage;
 
         validateUrl(url);
         this.url = url;
 
         validatePublicId(publicId);
-        this.publicId = publicId;
-    }
-
-    public static AnnouncementAttachment create(
-            String url,
-            String publicId,
-            AttachmentType type
-    ) {
-        return new AnnouncementAttachment(
-                UUID.randomUUID(),
-                url,
-                publicId,
-                type,
-                false
-        );
-    }
-
-    public static AnnouncementAttachment reconstitute(
-            UUID attachmentId,
-            String url,
-            String publicId,
-            AttachmentType type,
-            boolean displayImage
-    ) {
-        return new AnnouncementAttachment(
-                attachmentId,
-                url,
-                publicId,
-                type,
-                displayImage
-        );
-    }
-
-    public void markAsDisplayImage() {
-        if (type != AttachmentType.IMAGE) {
-            throw new InvalidDisplayImageTypeException(type);
-        }
-        this.displayImage = true;
-    }
-
-    public void unmarkAsDisplayImage() {
-        this.displayImage = false;
     }
 
     private static void validateUrl(String url) {
@@ -95,15 +48,8 @@ public final class AnnouncementAttachment {
         return url;
     }
 
-    public String publicId() {
-        return publicId;
-    }
-
     public AttachmentType type() {
         return type;
     }
 
-    public boolean isDisplayImage() {
-        return displayImage;
-    }
 }
