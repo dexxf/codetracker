84f0ba4ea7a325a7428e6b3c90bb542e5e69e420
Dexter Facelo <Dexter101306@gmail.com>
2026-08-01 00:05:26 +0800

    refactor: rename SubmitTrackedTrackedActivityService to SubmitTrackedActivityService

--- src/main/java/com/io/codetracker/application/activity/service/SubmitTrackedActivityService.java
+++ src/main/java/com/io/codetracker/application/activity/service/SubmitTrackedActivityService.java
@@ -18,7 +18,7 @@ import java.util.UUID;
 
 @Service
 @AllArgsConstructor
-public class SubmitTrackedTrackedActivityService implements SubmitTrackedActivityUseCase {
+public class SubmitTrackedActivityService implements SubmitTrackedActivityUseCase {
 
     private final StudentActivityAppRepository studentActivityAppRepository;
     private final ActivityClassroomAppPort activityClassroomAppPort;
