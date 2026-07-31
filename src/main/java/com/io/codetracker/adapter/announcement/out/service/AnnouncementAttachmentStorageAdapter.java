d87c22ea8e2f1d020d5c3b92656bbf3ca574a67e
Dexter Facelo <Dexter101306@gmail.com>
2026-07-31 12:11:46 +0800

    feat: add folder path for announcements

--- src/main/java/com/io/codetracker/adapter/announcement/out/service/AnnouncementAttachmentStorageAdapter.java
+++ src/main/java/com/io/codetracker/adapter/announcement/out/service/AnnouncementAttachmentStorageAdapter.java
@@ -3,7 +3,7 @@ package com.io.codetracker.adapter.announcement.out.service;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.io.codetracker.application.announcement.port.out.AnnouncementAttachmentStoragePort;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -11,22 +11,29 @@ import java.util.Map;
 import java.util.UUID;
 
 @Service
-@AllArgsConstructor
 public class AnnouncementAttachmentStorageAdapter implements AnnouncementAttachmentStoragePort {
 
     private final Cloudinary cloudinary;
+    private final String announcementFolderPath;
+
+    public AnnouncementAttachmentStorageAdapter(
+            Cloudinary cloudinary,
+            @Value("${announcement.attachment.folder-path}") String announcementFolderPath) {
+        this.cloudinary = cloudinary;
+        this.announcementFolderPath = announcementFolderPath;
+    }
 
     @Override
     public UploadedAttachment upload(byte[] content, UUID classroomId, UUID attachmentId) throws IOException {
 
         Map<String, Object> options = ObjectUtils.asMap(
                 "public_id", attachmentId.toString(),
-                "folder", classroomId.toString(),
+                "folder", announcementFolderPath + classroomId.toString(),
                 "resource_type", "auto",
                 "overwrite", false
         );
 
-        Map uploadResult = cloudinary.uploader().upload(content, options);
+        Map<String, Object> uploadResult = cloudinary.uploader().upload(content, options);
 
         return new UploadedAttachment(
                 (String) uploadResult.get("secure_url"),
@@ -36,7 +43,7 @@ public class AnnouncementAttachmentStorageAdapter implements AnnouncementAttachm
 
     @Override
     public void delete(UUID classroomId, UUID publicId, String resourceType) throws IOException {
-        String fullPublicId = classroomId + "/" + publicId;
+        String fullPublicId = announcementFolderPath + classroomId + "/" + publicId;
         cloudinary.uploader().destroy(fullPublicId, ObjectUtils.asMap("resource_type", resourceType, "type", "upload","invalidate", true));
     }
 }
