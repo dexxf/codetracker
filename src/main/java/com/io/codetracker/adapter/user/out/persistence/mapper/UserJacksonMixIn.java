ef930d8fc3be23e56e936ffa42e5809901b97813
Dexter Facelo <Dexter101306@gmail.com>
2026-08-08 14:25:46 +0800

    refactor: remove jsonProperty in UserJacksonMixIn

--- src/main/java/com/io/codetracker/adapter/user/out/persistence/mapper/UserJacksonMixIn.java
+++ src/main/java/com/io/codetracker/adapter/user/out/persistence/mapper/UserJacksonMixIn.java
@@ -1,7 +1,6 @@
 package com.io.codetracker.adapter.user.out.persistence.mapper;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.io.codetracker.domain.user.entity.User;
 import com.io.codetracker.domain.user.valueobject.Gender;
 
@@ -11,12 +10,12 @@ public abstract class UserJacksonMixIn {
 
     @JsonCreator
     static User reconstitute(
-            @JsonProperty("userId") UUID userId,
-            @JsonProperty("firstName") String firstName,
-            @JsonProperty("lastName") String lastName,
-            @JsonProperty("gender") Gender gender,
-            @JsonProperty("profileUrl") String profileUrl,
-            @JsonProperty("hasFullyInitialized") boolean hasFullyInitialized) {
+            UUID userId,
+            String firstName,
+            String lastName,
+            Gender gender,
+            String profileUrl,
+            boolean hasFullyInitialized) {
         return User.reconstitute(userId, firstName, lastName, gender, profileUrl, hasFullyInitialized);
     }
 }
