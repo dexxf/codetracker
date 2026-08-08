de006b57394c3ff863c7165c1764136c111d6119
Dexter Facelo <Dexter101306@gmail.com>
2026-08-09 01:15:04 +0800

    feat: cache classroom settings domain repository impl

--- src/main/java/com/io/codetracker/adapter/classroom/out/persistence/repository/ClassroomSettingsDomainRepositoryImpl.java
+++ src/main/java/com/io/codetracker/adapter/classroom/out/persistence/repository/ClassroomSettingsDomainRepositoryImpl.java
@@ -1,12 +1,14 @@
 package com.io.codetracker.adapter.classroom.out.persistence.repository;
 
 import com.io.codetracker.adapter.classroom.out.persistence.mapper.ClassroomSettingsMapper;
+import com.io.codetracker.common.cache.CacheNames;
 import com.io.codetracker.domain.classroom.entity.ClassroomSettings;
 import com.io.codetracker.domain.classroom.repository.ClassroomSettingsDomainRepository;
 import com.io.codetracker.infrastructure.classroom.persistence.entity.ClassroomSettingsEntity;
 import com.io.codetracker.infrastructure.classroom.persistence.repository.JpaClassroomSettingsRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -18,6 +20,7 @@ public class ClassroomSettingsDomainRepositoryImpl implements ClassroomSettingsD
     private final JpaClassroomSettingsRepository jpaClassroomSettingsRepository;
 
     @Override
+    @Cacheable(value = CacheNames.CLASSROOM_SETTINGS, key = "#classroomId", unless = "#result.isEmpty()")
     public Optional<ClassroomSettings> findByClassroomId(UUID classroomId) {
         Optional<ClassroomSettingsEntity> classroomSettingsEntity = jpaClassroomSettingsRepository.findByClassroomId(classroomId);
         return classroomSettingsEntity.map(ClassroomSettingsMapper::toDomain);
