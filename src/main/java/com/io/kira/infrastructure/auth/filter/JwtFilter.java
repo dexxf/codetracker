bd5e88bd8abefc6d3e9d0fce735b2171e1cfbf32
Dexter Facelo <Dexter101306@gmail.com>
2026-08-12 18:10:30 +0800

    fix: skip JwtFilter for refresh endpoint

--- src/main/java/com/io/kira/infrastructure/auth/filter/JwtFilter.java
+++ src/main/java/com/io/kira/infrastructure/auth/filter/JwtFilter.java
@@ -83,4 +83,9 @@ public class JwtFilter extends OncePerRequestFilter {
         filterChain.doFilter(request,response);
     }
 
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getServletPath().equals("/api/auth/refresh");
+    }
+
 }
